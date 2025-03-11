a4b25149c8005e19b052e292397a7ee988afb9c7
Takashi Itoh <takashi.ito@jst.go.jp>
2025-03-12 07:16:33 +0900

    Remove main() for test
    Signed-off-by: Takashi Itoh <takashi.ito@jst.go.jp>

--- cabot_dashboard_server/app/services/github.py
+++ cabot_dashboard_server/app/services/github.py
@@ -15,15 +15,3 @@ async def fetchSiteReleases(repository):
             "CABOT_SITE_VERSION": [result["tag_name"] for result in response.json()],
             "CABOT_SITE": f"{os.path.basename(repository).replace('_sites_','_site_')}_3d",
         }
-
-
-if __name__ == "__main__":
-    import asyncio
-    from pprint import pprint
-
-    async def main():
-        pprint(await fetchSiteReleases("cabot_sites_cmu"))
-        pprint(await fetchSiteReleases("cmu-cabot/cabot_sites_miraikan"))
-        pprint(await fetchSiteReleases("cabot_sites_expo2025"))
-
-    asyncio.run(main())
