85927414d8e64fcfda820cf10450505c82d2b5dd
Daisuke Sato <daisukes@cmu.edu>
2025-04-10 13:48:22 +0900

    update host-setup.sh
    Signed-off-by: Daisuke Sato <daisukes@cmu.edu>

--- cabot_dashboard_client/host-setup.sh
+++ cabot_dashboard_client/host-setup.sh
@@ -16,20 +16,21 @@ echo site-version:$CABOT_SITE_VERSION
 systemctl --user stop cabot
 
 echo Setup dependency
+git fetch -p
 git checkout $CABOT_LAUNCH_IMAGE_TAG
 (./setup-dependency.sh)
 echo Done
 
-echo イメージとマップの取得
-rm -rf $CABOT_SITE_PKG_DIR
+echo Getting docker images and map data
 mkdir -p $CABOT_SITE_PKG_DIR
+rm -rf $CABOT_SITE_PKG_DIR/$CABOT_SITE_REPO  # delete pkg only, do not delete downloaded zip
 (./manage-pkg.sh -r $CABOT_SITE_REPO -v $CABOT_SITE_VERSION -d -u)
-# CABOT_SITE_PKG_DIRに依存。ないとpwdにダウンロード
+# depends on CABOT_SITE_PKG_DIR, download to the current working dir if not exisit
 echo Done
 
 (./manage-pkg.sh -p $CABOT_LAUNCH_IMAGE_TAG)
 
-echo セットアップとインストール
-(./plugin-build.sh)
-systemctl --user restart cabot-plugin
+echo Set up and Install
+(./build-workspace.sh -o)  # build host ros workspace
+(./plugin-build.sh -s)
 echo All Done
