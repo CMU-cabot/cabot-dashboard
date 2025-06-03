b402fa99961b110d2d23e3f74a3bc9f295040926
Takashi Itoh <takashi.ito@jst.go.jp>
2025-06-03 14:36:58 +0900

    Update command line
    Signed-off-by: Takashi Itoh <takashi.ito@jst.go.jp>

--- cabot_dashboard_client/remote-exec.sh
+++ cabot_dashboard_client/remote-exec.sh
@@ -14,7 +14,7 @@ case $1 in
     ros-stop)
         args="systemctl --user stop cabot";;
     report-submit)
-        args="~/cabot_ws/cabot/submit_report.sh ${@:2}";;
+        args="cd /opt/report-submitter && docker compose run --rm log ./submit_report_local.sh ${@:2}";;
     system-reboot)
         args="sudo systemctl reboot";;
         # want to reboot the entire system, but not implemented yet
